d7af8415e8507002d785e799b4e89999af506b34
Debashish.Rath <debashish.rath@mphatek.com>
2022-12-13 04:20:42 +0530

    done vercel setting

--- tcs_site/settings.py
+++ tcs_site/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure---j&z(rsxseng111(!j953rnfganb+$@qrk^3vw%x-6#dc2dca
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app', '.now.sh']
 
 
 # Application definition
@@ -118,6 +118,8 @@ STATIC_URL = "static/"
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR,'static')
 ]
+
+STATIC_ROOT = os.path.join(BASE_DIR,"staticfiles_build","static")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
