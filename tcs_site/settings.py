2d276b0ea29098e04ee4b58325cbb81321cab550
Debashish.Rath <debashish.rath@mphatek.com>
2022-12-13 20:14:45 +0530

    config for railway deployment

--- tcs_site/settings.py
+++ tcs_site/settings.py
@@ -127,3 +127,10 @@ STATIC_ROOT = os.path.join(BASE_DIR,"staticfiles")
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
+import dj_database_url
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+DATABASES = {
+    "default": dj_database_url.config(default=DATABASE_URL, conn_max_age=1800),
+}
