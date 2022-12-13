444b6f5adb1423f08776e587de19cd8154db493b
Debashish.Rath <debashish.rath@mphatek.com>
2022-12-14 03:00:46 +0530

    done modification in csrf settings of project

--- tcs_site/settings.py
+++ tcs_site/settings.py
@@ -138,3 +138,5 @@ DATABASE_URL = os.getenv("DATABASE_URL")
 DATABASES = {
     "default": dj_database_url.config(default=DATABASE_URL, conn_max_age=1800),
 }
+
+CSRF_TRUSTED_ORIGINS = ['https://team-cricketing-spirits.up.railway.app']
