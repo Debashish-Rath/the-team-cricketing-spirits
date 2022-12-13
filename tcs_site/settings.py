3ba2d37eb17cf61218d7078aa62646fd1d28ca18
Debashish.Rath <debashish.rath@mphatek.com>
2022-12-13 21:20:15 +0530

    modified project settings

--- tcs_site/settings.py
+++ tcs_site/settings.py
@@ -16,7 +16,7 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 load_dotenv(os.path.join(BASE_DIR, ".env"))
-SECRET_KEY = os.getenv("SECRET_KEY")
+SECRET_KEY = str(os.getenv("SECRET_KEY"))
 print("SECRET_KEY: ", SECRET_KEY)
 
 
