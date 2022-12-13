26341e2e42b9320687891d74b3e3974868d43772
Debashish.Rath <debashish.rath@mphatek.com>
2022-12-13 13:31:01 +0530

    done settings for deployment

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn tcs_site.wsgi --log-file -
--- tcs_site/settings.py
+++ tcs_site/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure---j&z(rsxseng111(!j953rnfganb+$@qrk^3vw%x-6#dc2dca
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app', '.now.sh']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -111,14 +111,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
 
 # Own Paths
-
-STATICFILES_DIRS = os.path.join(BASE_DIR,'static')
-
-
-STATIC_ROOT = os.path.join(BASE_DIR,"staticfiles_build","static")
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR,'static'),
+    ]
+STATIC_ROOT = os.path.join(BASE_DIR,"staticfiles")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
