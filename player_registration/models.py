5ed43abf2f49d1b2bb47e42afc58ee8c82f62a95
Debashish.Rath <debashish.rath@mphatek.com>
2022-12-14 02:48:49 +0530

    done modification in models.py file and insertion of data into database

--- player_registration/models.py
+++ player_registration/models.py
@@ -2,4 +2,12 @@ from django.db import models
 
 # Create your models here.
 class Player_Registration_Form(models.Model):
-    pass
+    name = models.CharField(max_length=80)
+    phone_no = models.BigIntegerField()
+    whatsapp_no = models.BigIntegerField()
+    role = models.CharField(max_length=50,default='LHB')
+    reference_person = models.CharField(max_length=80)
+    is_available = models.BooleanField()
+
+    def __str__(self):
+        return self.name
--- player_registration/views.py
+++ player_registration/views.py
@@ -1,8 +1,27 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from player_registration.models import Player_Registration_Form
 
 # Create your views here.
 
 
 def player_register(request):
-    return render(request, 'register.html')
+    if request.method == 'POST':
+        name = request.POST['name']
+        phone_no = request.POST['phone_no']
+        whatsapp_no = request.POST['whatsapp_no']
+        reference_person = request.POST['reference_person']
+        is_available = request.POST['availability']
+
+        
+        Player_Registration_Form.objects.create(name=name, phone_no=phone_no,whatsapp_no=whatsapp_no,role='LHB',reference_person=reference_person,is_available=True)
+
+        # print(player_registeration_form)
+        print(name)
+        print(phone_no)
+        print(whatsapp_no)
+        print(reference_person)
+        print(is_available)
+        return HttpResponse("Successfully Registered")
+    else:
+        return render(request, 'register.html')
